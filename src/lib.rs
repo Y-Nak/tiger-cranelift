4a2fb7c2d623d39e39b1e1e9c775b14fc14bff3a
Y-Nak <yurayura.rounin.3@gmail.com>
2020-01-24 17:44:05 +0900

    Add error type

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
+pub mod error;
 mod intern;
 mod pos;
 
+pub type Result<T> = std::result::Result<T, error::Error>;
+
 mod impl_prelude {
     use super::*;
+
+    pub use super::Result;
+    pub use error::Error;
     pub use intern::{kw, Symbol};
-    pub use pos::{Cursor, Pos};
+    pub use pos::{BytePos, Cursor, Pos};
 }
